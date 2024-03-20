285a416740092273dd0732bf6362e1158d3fa025
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-03-20 17:19:03 +0000

    Added Section Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/button";
 import Header from "./components/header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Section from "./section";
+
+const Hero = () => {
+  return <Section>Hero</Section>;
+};
+
+export default Hero;
