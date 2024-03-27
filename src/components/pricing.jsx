d197f28a1c12795b1214b3dd1f0039a81839e39e
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-03-27 17:22:42 +0000

    added CTA to pricing

--- src/components/pricing.jsx
+++ src/components/pricing.jsx
@@ -2,6 +2,7 @@ import { smallSphere, stars } from "../assets";
 import Heading from "./heading";
 import PricingList from "./pricingList";
 import Section from "./section";
+import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
   return (
@@ -32,6 +33,16 @@ const Pricing = () => {
 
         <div className="relative">
           <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+        <div className="flex justify-center mt-10">
+          <a
+            className="text-xs font-code font-bold tracking-wider uppercase border-b"
+            href="/pricing"
+          >
+            See the full details
+          </a>
         </div>
       </div>
     </Section>
